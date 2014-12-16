4de2616b5ad7e898e0d10004672019de6ed79e6f
zalaare <zalaare@gmail.com>
2014-12-16 16:11:20 +0000

    [mali-x11] bump version

--- packages/graphics/mali-x11/package.mk
+++ packages/graphics/mali-x11/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mali-x11"
-PKG_VERSION="1.2"
+PKG_VERSION="1.3"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
