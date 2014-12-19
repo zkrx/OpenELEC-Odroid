c1fdeb9aafdf6bcbbc582799af3168d925e6ddc5
zalaare <zalaare@gmail.com>
2014-12-19 20:22:52 +0000

    [mali-x11] version bump switch to r4p1

--- packages/graphics/mali-x11/package.mk
+++ packages/graphics/mali-x11/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mali-x11"
-PKG_VERSION="1.3"
+PKG_VERSION="1.4"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
