33816b4b5267bcc4bc1d4ce98ae72f70f7e8154e
zalaare <zalaare@gmail.com>
2015-01-28 15:03:52 +0000

    [xf86-video-armsoc] move device specific package to project package

--- projects/Odroid/packages/xf86-video-armsoc/package.mk
+++ projects/Odroid/packages/xf86-video-armsoc/package.mk
@@ -17,7 +17,10 @@
 ################################################################################
 
 PKG_NAME="xf86-video-armsoc"
-PKG_VERSION="29356f5"
+case $DEVICE in
+  U2) PKG_VERSION="29356f5" ;;
+  XU3) PKG_VERSION="1.1.0" ;;
+esac
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE=""
