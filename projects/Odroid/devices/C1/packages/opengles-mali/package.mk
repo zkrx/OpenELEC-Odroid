c0130f097d21a341554ddcf1c303fae93d61a60d
Zalaare <zalaare@gmail.com>
2015-02-01 16:45:27 -0500

    [opengles-mali] use odroid-c1 specific mali blobs

--- projects/Odroid/devices/C1/packages/opengles-mali/package.mk
+++ projects/Odroid/devices/C1/packages/opengles-mali/package.mk
@@ -17,25 +17,20 @@
 ################################################################################
 
 PKG_NAME="opengles-mali"
-PKG_VERSION="2.0+C1_fb"
+PKG_VERSION="1216b9e"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://openlinux.amlogic.com:8000/download/ARM/filesystem/"
-PKG_URL="http://sources.openelec.tv/devel/opengl-meson6-r4p0-01-armhf.tar.xz"
+PKG_URL="$ODROID_MIRROR/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libamcodec"
 PKG_PRIORITY="optional"
 PKG_SECTION="graphics"
 PKG_SHORTDESC="OpenGL ES pre-compiled libraries for Mali 400 GPUs found in Amlogic Meson6 SoCs"
 PKG_LONGDESC="OpenGL ES pre-compiled libraries for Mali 400 GPUs found in Amlogic Meson6 SoCs. The libraries could be found in a Linux buildroot released by Amlogic at http://openlinux.amlogic.com:8000/download/ARM/filesystem/. See the opengl package."
-PKG_SOURCE_DIR="$(basename ${PKG_URL/.tar.xz})"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
-
-unpack() {
-  $SCRIPTS/extract $PKG_NAME $(basename $PKG_URL) $BUILD
-}
 
 make_target() {
  : nothing
@@ -43,11 +38,11 @@ make_target() {
 
 makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/lib
-    cp -PR usr/lib/*.so* $SYSROOT_PREFIX/usr/lib
+    cp -PR fbdev/mali_libs/*.so* $SYSROOT_PREFIX/usr/lib
   mkdir -p $SYSROOT_PREFIX/usr/include
-    cp -PR usr/include/* $SYSROOT_PREFIX/usr/include
+    cp -PR fbdev/mali_headers/* $SYSROOT_PREFIX/usr/include
   mkdir -p $INSTALL/usr/lib
-    cp -PR usr/lib/*.so* $INSTALL/usr/lib
+    cp -PR fbdev/mali_libs/*.so* $INSTALL/usr/lib
 }
 
 post_install() {
