4520401f90442df5f53fb61b3139c9d99f67bbbd
zalaare <zalaare@gmail.com>
2015-05-04 15:24:25 +0000

    Revert "[opengles-mali] revert Mali for Odroid C1 to r4p0"   Not ready for this yet.

--- projects/Odroid/devices/C1/packages/opengles-mali/package.mk
+++ projects/Odroid/devices/C1/packages/opengles-mali/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="opengles-mali"
-PKG_VERSION="1216b9e"
+PKG_VERSION="a2bca98"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
